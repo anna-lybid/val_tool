fbc968e555d0a827691357a7263ced9c4c5fecd9
anna-lybid <anna.lybid@gmail.com>
2024-02-24 14:33:17 +0200

    Initial commit

---
+++ process/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Bus(models.Model):
+    info = models.CharField(max_length=255, null=True)
+    num_seat = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "buses"
+
+    def __str__(self):
+        return str(self.info)
