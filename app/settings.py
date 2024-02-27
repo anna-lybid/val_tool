6d37bf3e99010a65e4241a966aa6c1634166e65d
Anna Lybid <alybid@MB-alybid-M1.local>
2024-02-27 13:55:10 +0200

    Add Product model

--- app/settings.py
+++ app/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path("/Users/alybid/PycharmProjects/web_app/val_tool")
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "process",
     "user",
 ]
@@ -77,9 +77,9 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- process/views.py
+++ process/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from process.models import Bus, Product
+from process.serializers import ProcessSerializer, ProductSerializer
+
+
+class ProcessViewSet(
+    viewsets.ModelViewSet
+):
+    queryset = Bus.objects.all()
+    serializer_class = ProcessSerializer
+    lookup_field = 'id'
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
